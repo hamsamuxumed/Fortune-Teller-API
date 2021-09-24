e23f86bb96052268c547005fd0109c7ad9aedb50
hamsamuxumed <hamsamuxumed3@gmail.com>
2021-09-24 11:20:14 +0100

    starter server code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const port = process.env.PORT || 2000;
 const app = express();
 
-//listen for request
+
 app.listen(port, () => {
     console.log(`We are LIVE at http://localhost:${port}`)
 })
